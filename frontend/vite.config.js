12dd792a5e272454bc8f8d7077fa23f13503ccbe
Sophia Jung <sophiabraddock82@gmail.com>
2026-06-14 17:40:45 -0400

    deployment fix 1001

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,11 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: ['mapbox-gl', '@mapbox/mapbox-gl-draw'],
+    },
+  },
   server: {
     port: 5173,
     proxy: {
